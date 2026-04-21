a17e74dfc6203d251d4fe36a1f23058761aa00fc
danbenosem <danielbenjami562@gmail.com>
2026-04-21 12:11:48 +0100

    tASK

--- TouchGrass/Kata.java
+++ TouchGrass/Kata.java
@@ -31,6 +31,23 @@ System.out.println(subResult);
 
 int quoResult= quotient(firstNumber, secondNumber);
 System.out.println(quoResult);
+
+int factorResult= factorOf(even);
+System.out.println(factorResult);
+
+
+boolean perfectResult= isPerfectSquare(even);
+System.out.println(perfectResult);
+
+
+boolean palin= isPalindrome(even);
+System.out.println(palin);
+
+long facto= factorial(even);
+System.out.println(facto);
+
+long squarevalue= squareOf(even);
+System.out.println(squarevalue);
 }
 
 
@@ -105,7 +122,66 @@ public static int quotient(int a, int b){
 
 }
 
+public static int factorOf(int a){
+int count=0;
+    for (int num=1; num<= a; num++){
+        if (a % num==0){
+            count++;
+            
+            }
+    
+        }
+
+        return count;
+}
+
+public static boolean isPerfectSquare(int a){
+    int square= (int) Math.sqrt(a);
+    if (square * square==a){
+
+        return true;
+    }
+    else {
+            return false;
+        }
+}
 
 
+public static boolean isPalindrome (int a){
+
+int digit1= a/10000;
+int digit2= (a %10000)/1000;
+int digit3= ((a%10000)%1000)/100;
+int digit4= (((a%10000)%1000)%100)/10;
+int digit5= (((a%10000)%1000)%100)%10;
+
+if (digit1==digit5){
+        return true;
+  }
+ else {
+        return false;
+    }   
+
+}
+
+
+public static long factorial (int a){
+   int facto=1 ;
+    for (int number=a; number>=1; number--){
+       facto*=number;
+
+    }
+
+    return facto;
+
+}
+
+public static long squareOf(int a){
+
+    int square = a * a;
+    return square;
+
+
+}
 
 }
